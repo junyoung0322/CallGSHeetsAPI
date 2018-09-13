cd32a0e5e256321e868d9c85617507d075365fa0
DESKTOP-EV7CVOO\jun <lee.junyoung@no1s.biz>
2018-09-14 00:05:42 +0900

    Add Variable for APIKEY

--- CallGSheets/src/CallSheetsAPI.java
+++ CallGSheets/src/CallSheetsAPI.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 public class CallSheetsAPI {
 	public static void main(String[] args) {
 		try {
+			String sAPIKey = "APIKey";
+			
 			StringBuilder sUrlString = new StringBuilder();
 			sUrlString.append("https://sheets.googleapis.com/v4/spreadsheets/") // API URL
 			.append("11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM") // シートID
@@ -14,22 +16,22 @@ public class CallSheetsAPI {
 			.append("sales!A1:E6") // 範囲
 			.append("?key=") // API Key パラメータ名
 			// APIキーに置き換えてください。
-			.append("ここにAPIキーを書いてください"); // API Key
+			.append(sAPIKey); // API Key
 			
 			URL oUrl = new URL(sUrlString.toString());
 			HttpURLConnection con = (HttpURLConnection) oUrl.openConnection();
 			con.setRequestMethod("GET");
 
 			BufferedReader bufferedReader = null;
-			ArrayList<String> resultSet = new ArrayList<String>();
+			ArrayList<String> lResultSet = new ArrayList<String>();
 
 			bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
 
 			// JSONデータのパーズ
-			resultSet = JsonUtil.parseJSONData(bufferedReader);
+			lResultSet = JsonUtil.parseJSONData(bufferedReader);
 
-			for (String str: resultSet) {
-				System.out.println(str);
+			for (String sStr: lResultSet) {
+				System.out.println(sStr);
 			}
 		}
 		catch (Exception e) {
